dc7916656e297bebe8eb725a3ea48f0f8c944ac4
ragibhasan894 <ragibhasan894@gmail.com>
2024-01-23 18:15:11 +0600

    model designing and migrations

---
+++ app/Constants/Status.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants;
+
+class Status
+{
+    public const ACTIVE = 'active';
+    public const PENDING = 'pending';
+    public const REMOVED = 'removed';
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->foreignId('role_id')->constrained('roles');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('status')->default(\App\Constants\Status::ACTIVE);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
